4e7a886a7ee22cc5da2ee552502750627874c730
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-06 20:13:54 +0300

    Setup
    All classes are in the same file for simplicity.

--- VisitorPattern/Program.cs
+++ VisitorPattern/Program.cs
@@ -6,7 +6,30 @@ namespace VisitorPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person();
+            person.BankAccounts.Add(new BankAccount { Amount = 2000, MonthlyInterest = 0.01 });
+            person.BankAccounts.Add(new BankAccount { Amount = 3500, MonthlyInterest = 0.02 });
+            person.RealEstates.Add(new RealEstate { EstimatedValue = 120000, MonthlyRent = 1000 });
+            person.Loans.Add(new Loan { AmountOwed = 50000, MonthlyPayment = 600 });
+
+            decimal netWorth = 0;
+
+            foreach (var bankAccount in person.BankAccounts)
+            {
+                netWorth += bankAccount.Amount;
+            }
+
+            foreach (var realEstate in person.RealEstates)
+            {
+                netWorth += realEstate.EstimatedValue;
+            }
+
+            foreach (var loan in person.Loans)
+            {
+                netWorth -= loan.AmountOwed;
+            }
+
+            Console.WriteLine(netWorth);
         }
     }
 }
---
+++ VisitorPattern/Visitor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace VisitorPattern
+{
+    public class Person
+    {
+        public Person()
+        {
+            BankAccounts = new List<BankAccount>();
+            RealEstates = new List<RealEstate>();
+            Loans = new List<Loan>();
+        }
+
+        public List<BankAccount> BankAccounts { get; set; }
+        public List<RealEstate> RealEstates { get; set; }
+        public List<Loan> Loans { get; set; }
+    }
+
+    public class BankAccount
+    {
+        public decimal Amount { get; set; }
+        public double MonthlyInterest { get; set; }
+    }
+
+    public class RealEstate
+    {
+        public decimal EstimatedValue { get; set; }
+        public decimal MonthlyRent { get; set; }
+    }
+
+    public class Loan
+    {
+        public decimal AmountOwed { get; set; }
+        public decimal MonthlyPayment { get; set; }
+    }
+}
