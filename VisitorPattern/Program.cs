b723923e1ec628119c7dc918f3a154c76073f412
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-06 20:30:56 +0300

    Implement the visitor pettern

--- VisitorPattern/Program.cs
+++ VisitorPattern/Program.cs
@@ -7,29 +7,18 @@ namespace VisitorPattern
         static void Main(string[] args)
         {
             var person = new Person();
-            person.BankAccounts.Add(new BankAccount { Amount = 2000, MonthlyInterest = 0.01 });
-            person.BankAccounts.Add(new BankAccount { Amount = 3500, MonthlyInterest = 0.02 });
-            person.RealEstates.Add(new RealEstate { EstimatedValue = 120000, MonthlyRent = 1000 });
-            person.Loans.Add(new Loan { AmountOwed = 50000, MonthlyPayment = 600 });
+            person.Assets.Add(new BankAccount { Amount = 2000, MonthlyInterest = 0.01 });
+            person.Assets.Add(new BankAccount { Amount = 3500, MonthlyInterest = 0.02 });
+            person.Assets.Add(new RealEstate { EstimatedValue = 120000, MonthlyRent = 1000 });
+            person.Assets.Add(new Loan { AmountOwed = 50000, MonthlyPayment = 600 });
 
-            decimal netWorth = 0;
+            var netWorthVisitor = new NetWorthVisitor();
+            var monthlyIncomeVisitor = new MonthlyIncomeVisitor();
+            person.Accept(netWorthVisitor);
+            person.Accept(monthlyIncomeVisitor);
 
-            foreach (var bankAccount in person.BankAccounts)
-            {
-                netWorth += bankAccount.Amount;
-            }
-
-            foreach (var realEstate in person.RealEstates)
-            {
-                netWorth += realEstate.EstimatedValue;
-            }
-
-            foreach (var loan in person.Loans)
-            {
-                netWorth -= loan.AmountOwed;
-            }
-
-            Console.WriteLine(netWorth);
+            Console.WriteLine(netWorthVisitor.Amount);
+            Console.WriteLine(monthlyIncomeVisitor.Amount);
         }
     }
 }
--- VisitorPattern/Visitor.cs
+++ VisitorPattern/Visitor.cs
@@ -2,35 +2,106 @@
 
 namespace VisitorPattern
 {
-    public class Person
+    public interface IVisitor
+    {
+        void Visit(BankAccount bankAccount);
+        void Visit(RealEstate realEstate);
+        void Visit(Loan loan);
+    }
+
+    public class NetWorthVisitor : IVisitor
+    {
+        public decimal Amount { get; set; }
+
+        public void Visit(BankAccount bankAccount)
+        {
+            Amount += bankAccount.Amount;
+        }
+
+        public void Visit(RealEstate realEstate)
+        {
+            Amount += realEstate.EstimatedValue;
+        }
+
+        public void Visit(Loan loan)
+        {
+            Amount -= loan.AmountOwed;
+        }
+    }
+
+    public class MonthlyIncomeVisitor : IVisitor
+    {
+        public decimal Amount { get; set; }
+
+        public void Visit(BankAccount bankAccount)
+        {
+            Amount += bankAccount.Amount * (decimal)bankAccount.MonthlyInterest;
+        }
+
+        public void Visit(RealEstate realEstate)
+        {
+            Amount += realEstate.MonthlyRent;
+        }
+
+        public void Visit(Loan loan)
+        {
+            Amount -= loan.MonthlyPayment;
+        }
+    }
+
+    public interface IAsset
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class Person : IAsset
     {
         public Person()
         {
-            BankAccounts = new List<BankAccount>();
-            RealEstates = new List<RealEstate>();
-            Loans = new List<Loan>();
+            Assets = new List<IAsset>();
         }
 
-        public List<BankAccount> BankAccounts { get; set; }
-        public List<RealEstate> RealEstates { get; set; }
-        public List<Loan> Loans { get; set; }
+        public List<IAsset> Assets { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (var asset in Assets)
+            {
+                asset.Accept(visitor);
+            }
+        }
     }
 
-    public class BankAccount
+    public class BankAccount : IAsset
     {
         public decimal Amount { get; set; }
         public double MonthlyInterest { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
 
-    public class RealEstate
+    public class RealEstate : IAsset
     {
         public decimal EstimatedValue { get; set; }
         public decimal MonthlyRent { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
 
-    public class Loan
+    public class Loan : IAsset
     {
         public decimal AmountOwed { get; set; }
         public decimal MonthlyPayment { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
 }
